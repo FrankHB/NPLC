e0e59c9e8b46d52e8451f2dea01ee549af9d67ec
FrankHB <frankhb1989@gmail.com>
2016-01-14 23:28:49 +0800

    NBuilder/NBuilder.cpp: 检查函数 add2 的参数个数。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/NBuilder.cpp
+++ NBuilder/NBuilder.cpp
@@ -11,13 +11,13 @@
 /*!	\file NBuilder.cpp
 \ingroup NBuilder
 \brief NPL 解释实现。
-\version r4318
+\version r4320
 \author FrankHB<frankhb1989@gmail.com>
 \since YSLib build 301
 \par 创建时间:
 	2011-07-02 07:26:21 +0800
 \par 修改时间:
-	2016-01-14 23:27 +0800
+	2016-01-14 23:28 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -494,6 +494,8 @@ LoadFunctions(NPLContext& context)
 				term.Value = to_string(e1 + e2);
 			}
 			CatchThrow(std::invalid_argument& e, LoggedEvent(e.what(), Warning))
+		else
+			ThrowArityMismatch(2, n);
 	});
 }
 
