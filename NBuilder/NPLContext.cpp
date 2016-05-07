f8fd6918e2a0e4860fb0b6b9e063583684adacaa
FrankHB <frankhb1989@gmail.com>
2016-05-07 18:57:58 +0800

    NBuilder/NPLContext.cpp: 修复 F 表达式求值策略。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/NPLContext.cpp
+++ NBuilder/NPLContext.cpp
@@ -11,13 +11,13 @@
 /*!	\file NPLContext.cpp
 \ingroup Adaptor
 \brief NPL 上下文。
-\version r2006
+\version r2007
 \author FrankHB <frankhb1989@gmail.com>
 \since YSLib build 329
 \par 创建时间:
 	2012-08-03 19:55:29 +0800
 \par 修改时间:
-	2016-04-27 15:56 +0800
+	2016-05-07 18:57 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -114,7 +114,7 @@ NPLContext::SetupRoot(EvaluationPasses passes)
 #if NPL_TraceDepth
 	SetupTraceDepth(root);
 #endif
-	passes += ReduceFirst,
+	passes += ReduceFirst;
 	passes += [](TermNode& term, ContextNode& ctx){
 		// TODO: Form evaluation: macro expansion, etc.
 		if(!term.empty())
