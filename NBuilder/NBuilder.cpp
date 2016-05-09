3e5246f04cb0d5b08656745d19a1af37adc038c5
FrankHB <frankhb1989@gmail.com>
2016-05-09 13:50:17 +0800

    NBuilder/NBuilder: 使用 YSLib 简化实现并使用 NPLException 表示元数不匹配的错误。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/NBuilder.cpp
+++ NBuilder/NBuilder.cpp
@@ -11,13 +11,13 @@
 /*!	\file NBuilder.cpp
 \ingroup NBuilder
 \brief NPL 解释实现。
-\version r4906
+\version r4912
 \author FrankHB<frankhb1989@gmail.com>
 \since YSLib build 301
 \par 创建时间:
 	2011-07-02 07:26:21 +0800
 \par 修改时间:
-	2016-05-08 17:37 +0800
+	2016-05-09 13:49 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -42,14 +42,6 @@ namespace NPL
 
 namespace A1
 {
-
-void
-ThrowArityMismatch(size_t expected, size_t received)
-{
-	throw LoggedEvent(ystdex::sfmt("Arity mismatch:"
-		" expected %zu, received %zu.", expected, received), Err);
-}
-
 
 bool
 ExtractModifier(TermNode::Container& con, const ValueObject& mod)
--- NBuilder/NBuilder.h
+++ NBuilder/NBuilder.h
@@ -11,13 +11,13 @@
 /*!	\file NBuilder.h
 \ingroup NBuilder
 \brief NPL 解释实现。
-\version r1954
+\version r1957
 \author FrankHB<frankhb1989@gmail.com>
 \since YSLib build 304
 \par 创建时间:
 	2012-04-23 15:25:02 +0800
 \par 修改时间:
-	2016-03-02 15:57 +0800
+	2016-05-09 13:49 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -38,11 +38,6 @@ using namespace YSLib;
 
 namespace A1
 {
-
-/// 673
-YB_NORETURN void
-ThrowArityMismatch(size_t, size_t);
-
 
 /// 674
 //@{
