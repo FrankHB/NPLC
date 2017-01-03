2acf55e3c53ccfa2680ffae569111f9daa2fb2a7
FrankHB <frankhb1989@gmail.com>
2017-01-03 15:33:47 +0800

    NBuilder/NPLContext.cpp: 允许以 '+' 或 '-' 起始的非字面量作为一般标识符。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/NPLContext.cpp
+++ NBuilder/NPLContext.cpp
@@ -1,5 +1,5 @@
 ﻿/*
-	© 2012-2016 FrankHB.
+	© 2012-2017 FrankHB.
 
 	This file is part of the YSLib project, and may only be used,
 	modified, and distributed under the terms of the YSLib project
@@ -11,13 +11,13 @@
 /*!	\file NPLContext.cpp
 \ingroup Adaptor
 \brief NPL 上下文。
-\version r2267
+\version r2270
 \author FrankHB <frankhb1989@gmail.com>
 \since YSLib build 329
 \par 创建时间:
 	2012-08-03 19:55:29 +0800
 \par 修改时间:
-	2016-12-26 11:22 +0800
+	2017-01-03 15:32 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -94,6 +94,8 @@ LoadDeafultLiteralPasses(ContextNode& ctx)
 					term.Value = std::numeric_limits<long double>::quiet_NaN();
 				else if(id == "-nan.t")
 					term.Value = -std::numeric_limits<long double>::quiet_NaN();
+				else if(f != '#')
+					return true;
 			}
 			else if(std::isdigit(f))
 			{
