3ab9e48bd9c95572409841e58999f1b3cfbc7a79
FrankHB <frankhb1989@gmail.com>
2017-01-03 15:52:02 +0800

    NBuilder/NBuilder: 简化和重新分组语法形式的实现（依赖 YSLib b758r1 ）。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/NBuilder.cpp
+++ NBuilder/NBuilder.cpp
@@ -1,5 +1,5 @@
 ﻿/*
-	© 2011-2016 FrankHB.
+	© 2011-2017 FrankHB.
 
 	This file is part of the YSLib project, and may only be used,
 	modified, and distributed under the terms of the YSLib project
@@ -11,13 +11,13 @@
 /*!	\file NBuilder.cpp
 \ingroup NBuilder
 \brief NPL 解释实现。
-\version r5592
+\version r5651
 \author FrankHB<frankhb1989@gmail.com>
 \since YSLib build 301
 \par 创建时间:
 	2011-07-02 07:26:21 +0800
 \par 修改时间:
-	2016-12-27 21:51 +0800
+	2017-01-03 15:51 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -109,60 +109,63 @@ LoadFunctions(REPLContext& context)
 
 	LoadSequenceSeparators(root, context.ListTermPreprocess);
 	LoadDeafultLiteralPasses(root);
+	// NOTE: Literal builtins.
 	RegisterLiteralSignal(root, "exit", SSignal::Exit);
 	RegisterLiteralSignal(root, "cls", SSignal::ClearScreen);
 	RegisterLiteralSignal(root, "about", SSignal::About);
 	RegisterLiteralSignal(root, "help", SSignal::Help);
 	RegisterLiteralSignal(root, "license", SSignal::License);
+	// NOTE: Literal expression forms.
 	RegisterFormContextHandler(root, "$quote", Quote, IsBranch);
 	RegisterFormContextHandler(root, "$quote1",
 		ystdex::bind1(QuoteN, 1), IsBranch);
+	// NOTE: Binding and control forms.
 	RegisterFormContextHandler(root, "$define",
 		std::bind(DefineOrSet, _1, _2, true), IsBranch);
+	RegisterFormContextHandler(root, "$if", If, IsBranch);
+	RegisterFormContextHandler(root, "$lambda", Lambda, IsBranch);
 	RegisterFormContextHandler(root, "$set",
 		std::bind(DefineOrSet, _1, _2, false), IsBranch);
-	RegisterFormContextHandler(root, "$lambda", Lambda, IsBranch);
-	RegisterFormContextHandler(root, "$if", If, IsBranch);
-	RegisterStrictUnary(root, "ifdef",
-		[](TermNode& term, const ContextNode& ctx){
-		return ystdex::call_value_or([&](string_view id){
-			return bool(LookupName(ctx, id));
-		}, AccessPtr<string>(term));
-	});
-	RegisterStrict(root, "display", ystdex::bind1(LogTree, Notice));
-	// NOTE: Examples.
+	// NOTE: Privmitive procedures.
+	RegisterFormContextHandler(root, "$and", And);
+	RegisterFormContextHandler(root, "$or", Or);
+	RegisterFormContextHandler(root, "begin", ReduceOrdered),
+	RegisterStrict(root, "eq?", EqualReference);
+	RegisterStrict(root, "eqv?", EqualValue);
+	RegisterFormContextHandler(root, "list",
+		static_cast<void(&)(TermNode&, ContextNode&)>(ReduceChildren));
+	context.Perform("$define (not x) eqv? x #f");
+	// NOTE: Arithmetic procedures.
 	// FIXME: Overflow?
-	RegisterStrict(root, "+", std::bind(DoIntegerNAryArithmetics<
-		ystdex::plus<>>, ystdex::plus<>(), 0, _1), IsBranch);
+	RegisterStrict(root, "+", std::bind(CallBinaryFold<int, ystdex::plus<>>,
+		ystdex::plus<>(), 0, _1), IsBranch);
 	// FIXME: Overflow?
-	RegisterStrict(root, "add2", std::bind(DoIntegerBinaryArithmetics<
-		ystdex::plus<>>, ystdex::plus<>(), _1), IsBranch);
+	RegisterStrict(root, "add2", std::bind(CallBinaryAs<int, ystdex::plus<>>,
+		ystdex::plus<>(), _1), IsBranch);
 	// FIXME: Underflow?
-	RegisterStrict(root, "-", std::bind(DoIntegerBinaryArithmetics<
-		ystdex::minus<>>, ystdex::minus<>(), _1), IsBranch);
+	RegisterStrict(root, "-", std::bind(CallBinaryAs<int, ystdex::minus<>>,
+		ystdex::minus<>(), _1), IsBranch);
 	// FIXME: Overflow?
-	RegisterStrict(root, "*", std::bind(DoIntegerNAryArithmetics<
+	RegisterStrict(root, "*", std::bind(CallBinaryFold<int,
 		ystdex::multiplies<>>, ystdex::multiplies<>(), 1, _1), IsBranch);
 	// FIXME: Overflow?
-	RegisterStrict(root, "multiply2", std::bind(DoIntegerBinaryArithmetics<
+	RegisterStrict(root, "multiply2", std::bind(CallBinaryAs<int,
 		ystdex::multiplies<>>, ystdex::multiplies<>(), _1), IsBranch);
 	RegisterStrict(root, "/", [](TermNode& term){
-		DoIntegerBinaryArithmetics([](int e1, int e2){
+		CallBinaryAs<int>([](int e1, int e2){
 			if(e2 != 0)
 				return e1 / e2;
 			throw std::domain_error("Runtime error: divided by zero.");
 		}, term);
 	}, IsBranch);
 	RegisterStrict(root, "%", [](TermNode& term){
-		DoIntegerBinaryArithmetics([](int e1, int e2){
+		CallBinaryAs<int>([](int e1, int e2){
 			if(e2 != 0)
 				return e1 % e2;
 			throw std::domain_error("Runtime error: divided by zero.");
 		}, term);
 	}, IsBranch);
-	RegisterStrictUnary<const string>(root, "echo", Echo);
-	RegisterStrict(root, "eval", ystdex::bind1(Eval, std::ref(context)));
-	RegisterStrict(root, "system", CallSystem);
+	// NOTE: I/O library.
 	RegisterStrictUnary<const string>(root, "ofs", [&](const string& path){
 		if(ifstream ifs{path})
 			return ifs;
@@ -173,7 +176,25 @@ LoadFunctions(REPLContext& context)
 		return std::istringstream(str);
 	});
 	RegisterStrictUnary<ifstream>(root, "parse-f", ParseStream);
+	RegisterStrictUnary<LexicalAnalyzer>(root, "parse-lex", ParseOutput);
 	RegisterStrictUnary<std::istringstream>(root, "parse-s", ParseStream);
+	RegisterStrictUnary<const string>(root, "put", [&](const string& str){
+		std::cout << EncodeArg(str);
+	});
+	RegisterStrictUnary<const string>(root, "puts", [&](const string& str){
+		std::cout << EncodeArg(str) << std::endl;
+	});
+	// NOTE: Interop procedures.
+	RegisterStrict(root, "display", ystdex::bind1(LogTree, Notice));
+	RegisterStrictUnary<const string>(root, "echo", Echo);
+	RegisterStrictUnary<const string>(root, "env-get", [&](const string& var){
+		string res;
+
+		FetchEnvironmentVariable(res, var.c_str());
+		return res;
+	});
+	RegisterStrict(root, "eval",
+		ystdex::bind1(Eval, std::ref(context)), IsBranch);
 	RegisterStrictUnary<const string>(root, "lex", [&](const string& unit){
 		LexicalAnalyzer lex;
 
@@ -181,42 +202,25 @@ LoadFunctions(REPLContext& context)
 			lex.ParseByte(c);
 		return lex;
 	});
-	RegisterStrictUnary<LexicalAnalyzer>(root, "parse-lex", ParseOutput);
-	RegisterStrictUnary<const int>(root, "itos", [](int x){
-		return to_string(x);
-	});
-	RegisterStrictUnary<const string>(root, "put", [&](const string& str){
-		std::cout << EncodeArg(str);
-	});
-	RegisterStrictUnary<const string>(root, "puts", [&](const string& str){
-		std::cout << EncodeArg(str) << std::endl;
-	});
-	RegisterStrictUnary<const string>(root, "strlen", [&](const string& str){
-		return int(str.length());
-	});
-	RegisterStrictUnary(root, "typeid", [](TermNode& term){
-		// FIXME: Get it work with %YB_Use_LightweightTypeID.
-		return std::type_index(term.Value.GetType());
-	});
 	RegisterStrictUnary<const std::type_index>(root, "nameof",
 		[](const std::type_index& ti){
 		return string(ti.name());
 	});
-	RegisterStrict(root, "eq?", EqualReference);
-	RegisterStrict(root, "eqv?", EqualValue);
-	RegisterStrictUnary<const string>(root, "env-get", [&](const string& var){
-		string res;
-
-		FetchEnvironmentVariable(res, var.c_str());
-		return res;
+	RegisterStrict(root, "system", CallSystem);
+	// NOTE: Type interop library.
+	RegisterStrictUnary(root, "typeid", [](TermNode& term){
+		// FIXME: Get it work with %YB_Use_LightweightTypeID.
+		return std::type_index(term.Value.GetType());
 	});
-	RegisterFormContextHandler(root, "$and", And);
-	RegisterFormContextHandler(root, "$or", Or);
-	RegisterFormContextHandler(root, "begin", ReduceOrdered),
-	RegisterFormContextHandler(root, "list",
-		static_cast<void(&)(TermNode&, ContextNode&)>(ReduceChildren));
-	context.Perform("$define not ($lambda (x) eqv? x #f)");
-	context.Perform("$define ptype ($lambda (x) puts (nameof (typeid(x))))");
+	context.Perform("$define (ptype x) puts (nameof (typeid(x)))");
+	// NOTE: String library.
+	RegisterStrictUnary<const int>(root, "itos", [](int x){
+		return to_string(x);
+	});
+	RegisterStrictUnary<const string>(root, "strlen", [&](const string& str){
+		return int(str.length());
+	});
+
 }
 
 } // unnamed namespace;
--- NBuilder/NBuilder.h
+++ NBuilder/NBuilder.h
@@ -1,5 +1,5 @@
 ﻿/*
-	© 2012-2016 FrankHB.
+	© 2012-2017 FrankHB.
 
 	This file is part of the YSLib project, and may only be used,
 	modified, and distributed under the terms of the YSLib project
@@ -11,13 +11,13 @@
 /*!	\file NBuilder.h
 \ingroup NBuilder
 \brief NPL 解释实现。
-\version r2035
+\version r2060
 \author FrankHB<frankhb1989@gmail.com>
 \since YSLib build 304
 \par 创建时间:
 	2012-04-23 15:25:02 +0800
 \par 修改时间:
-	2016-12-17 22:27 +0800
+	2017-01-03 15:27 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -42,36 +42,6 @@ namespace A1
 /// 674
 void
 RegisterLiteralSignal(ContextNode&, const string&, SSignal);
-
-
-/// 696
-//@{
-template<typename _func>
-void
-DoIntegerBinaryArithmetics(_func f, TermNode& term)
-{
-	Forms::QuoteN(term, 2);
-
-	auto i(term.begin());
-	const int e1(Access<int>(Deref(++i)));
-
-	term.Value = f(e1, Access<int>(Deref(++i)));
-}
-
-template<typename _func>
-void
-DoIntegerNAryArithmetics(_func f, int val, TermNode& term)
-{
-	const auto n(Forms::FetchArgumentN(term));
-	auto i(term.begin());
-	const auto j(ystdex::make_transform(++i, [](TNIter i){
-		return Access<int>(Deref(i));
-	}));
-
-	// FIXME: Overflow?
-	term.Value = std::accumulate(j, std::next(j, n), val, f);
-}
-//@}
 
 } // namespace A1;
 
