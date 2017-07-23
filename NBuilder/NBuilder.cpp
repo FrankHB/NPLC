91dbbbba9d2b1dad72d7c83e403faf5bfe0a3d61
FrankHB <frankhb1989@gmail.com>
2017-07-23 15:15:53 +0800

    NBuilder/NBuilder.cpp: 简化函数 ptype 的实现。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/NBuilder.cpp
+++ NBuilder/NBuilder.cpp
@@ -11,13 +11,13 @@
 /*!	\file NBuilder.cpp
 \ingroup NBuilder
 \brief NPL 解释实现。
-\version r6516
+\version r6517
 \author FrankHB<frankhb1989@gmail.com>
 \since YSLib build 301
 \par 创建时间:
 	2011-07-02 07:26:21 +0800
 \par 修改时间:
-	2017-07-23 15:13 +0800
+	2017-07-23 15:15 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -376,7 +376,7 @@ LoadFunctions(REPLContext& context)
 		return std::type_index(term.Value.GetType());
 	});
 	// TODO: Copy of operand cannot be used for move-only types.
-	context.Perform("$defl! ptype (x) puts (nameof (typeid(x)))");
+	context.Perform("$defl! ptype (x) puts (nameof (typeid x))");
 	RegisterStrictUnary<string>(root, "get-typeid",
 		[&](const string& str) -> std::type_index{
 		if(str == "bool")
