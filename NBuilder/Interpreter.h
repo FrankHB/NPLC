558a808b50f6c7ab266dd7dcca240b289c797be1
FrankHB <frankhb1989@gmail.com>
2017-09-03 02:01:18 +0800

    NBuilder/Interpreter: 修复 LogTermValue 对项的非预期的修改。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/Interpreter.h
+++ NBuilder/Interpreter.h
@@ -11,13 +11,13 @@
 /*!	\file Interpreter.h
 \ingroup NBuilder
 \brief NPL 解释器。
-\version r150
+\version r152
 \author FrankHB <frankhb1989@gmail.com>
 \since YSLib build 403
 \par 创建时间:
 	2013-05-09 17:23:17 +0800
 \par 修改时间:
-	2017-08-04 09:51 +0800
+	2017-09-03 01:59 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -60,9 +60,9 @@ enum class SSignal
 void
 LogTree(const ValueNode&, Logger::Level = YSLib::Debug);
 
-/// 800
+/// 803
 void
-LogTermValue(TermNode&, Logger::Level = YSLib::Debug);
+LogTermValue(const TermNode&, Logger::Level = YSLib::Debug);
 
 /*!
 \build 控制台默认颜色。
