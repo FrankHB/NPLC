2ce30973b50fb5f97fa61242c76aa27b97224498
FrankHB <frankhb1989@gmail.com>
2020-03-07 21:39:05 +0800

    NBuilder/: 转移输出提示消息实现。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/Interpreter.cpp
+++ NBuilder/Interpreter.cpp
@@ -11,13 +11,13 @@
 /*!	\file Interpreter.cpp
 \ingroup NBuilder
 \brief NPL 解释器。
-\version r1215
+\version r1218
 \author FrankHB <frankhb1989@gmail.com>
 \since YSLib build 403
 \par 创建时间:
 	2013-05-09 17:23:17 +0800
 \par 修改时间:
-	2020-03-03 19:36 +0800
+	2020-03-07 21:38 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -672,9 +672,6 @@ Interpreter::Interpreter(Application& app,
 	p_env.reset(new YSLib::Environment(app));
 	loader(context);
 	terminal.UpdateForeColor(InfoColor);
-	cout << "Type \"exit\" to exit,"
-		" \"cls\" to clear screen, \"help\", \"about\", or \"license\""
-		" for more information." << endl << endl;
 }
 
 void
