fad91c22f31a3a5f64035f1667ccb0ef68752f20
FrankHB <frankhb1989@gmail.com>
2020-03-07 22:56:27 +0800

    NBuilder/Interpreter: 添加单行翻译单元处理接口。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/Interpreter.cpp
+++ NBuilder/Interpreter.cpp
@@ -11,13 +11,13 @@
 /*!	\file Interpreter.cpp
 \ingroup NBuilder
 \brief NPL 解释器。
-\version r1248
+\version r1254
 \author FrankHB <frankhb1989@gmail.com>
 \since YSLib build 403
 \par 创建时间:
 	2013-05-09 17:23:17 +0800
 \par 修改时间:
-	2020-03-07 22:09 +0800
+	2020-03-07 22:53 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -731,6 +731,13 @@ Interpreter::Process()
 		}
 	}
 	return true;
+}
+
+bool
+Interpreter::ProcessLine(string unit)
+{
+	line = std::move(unit);
+	return Process();
 }
 
 bool
