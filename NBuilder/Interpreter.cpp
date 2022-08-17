faad197482e63d1106df312e3567413d9b4fa282
FrankHB <frankhb1989@gmail.com>
2022-08-18 05:09:42 +0800

    NBuilder/Interpreter.cpp: EvaluateFastNonListCore: 调整分配器的使用。
    除注释的例外，和当前 YSLib 对分配器使用的约定一致。
    
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/Interpreter.cpp
+++ NBuilder/Interpreter.cpp
@@ -11,13 +11,13 @@
 /*!	\file Interpreter.cpp
 \ingroup NBuilder
 \brief NPL 解释器。
-\version r3492
+\version r3498
 \author FrankHB <frankhb1989@gmail.com>
 \since YSLib build 403
 \par 创建时间:
 	2013-05-09 17:23:17 +0800
 \par 修改时间:
-	2022-08-06 01:22 +0800
+	2022-08-18 05:09 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -850,12 +850,15 @@ YB_FLATTEN void
 EvaluateFastNonListCore(TermNode& term, const shared_ptr<Environment>& p_env,
 	TermNode& bound, ResolvedTermReferencePtr p_ref)
 {
+	const auto a(term.get_allocator());
+
+	// XXX: Some uses of allocator are different to YSLib for performance.
 	if(p_ref)
-		term.SetContent(bound.GetContainer(), ValueObject(std::allocator_arg,
-			term.get_allocator(), in_place_type<TermReference>,
-			p_ref->GetTags() & ~TermTags::Unique, *p_ref));
+		term.SetContent(bound.GetContainer(), ValueObject(std::allocator_arg, a,
+			in_place_type<TermReference>, p_ref->GetTags() & ~TermTags::Unique,
+			*p_ref));
 	else
-		term.SetValue(in_place_type<TermReference>,
+		term.SetValue(a, in_place_type<TermReference>,
 			NPL::Deref(p_env).MakeTermTags(bound) & ~TermTags::Unique, bound,
 			EnvironmentReference(p_env, NPL::Deref(p_env).GetAnchorPtr()));
 }
