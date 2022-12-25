62259aae73b9b4367c9b86cb46c2276e8cafc13f
FrankHB <frankhb1989@gmail.com>
2022-12-25 18:59:13 +0800

    Readme.zh-CN.md: 更新发布注记。
    Signed-off-by: FrankHB <frankhb1989@gmail.com>

--- NBuilder/NBuilder.cpp
+++ NBuilder/NBuilder.cpp
@@ -11,13 +11,13 @@
 /*!	\file NBuilder.cpp
 \ingroup NBuilder
 \brief NPL 解释实现。
-\version r8922
+\version r8923
 \author FrankHB<frankhb1989@gmail.com>
 \since YSLib build 301
 \par 创建时间:
 	2011-07-02 07:26:21 +0800
 \par 修改时间:
-	2022-12-18 19:37 +0800
+	2022-12-25 18:57 +0800
 \par 文本编码:
 	UTF-8
 \par 模块名称:
@@ -726,7 +726,7 @@ PrintHelpMessage(const string& prog)
 
 
 #define NPLC_NAME "NPL console"
-#define NPLC_VER "V1.4+ b961+"
+#define NPLC_VER "V1.5 b962"
 #if YCL_Win32
 #	define NPLC_PLATFORM "[MinGW32]"
 #elif YCL_Linux
